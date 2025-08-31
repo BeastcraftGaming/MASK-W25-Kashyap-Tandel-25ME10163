bd48d93cd869aa0d728b0e1c6088a6a4e226cac2
Kashyap Tandel <tandelkashyapt6@gmail.com>
2025-08-31 10:40:19 +0530

    remove node_modules

---
+++ B01-BAM/index.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+
+// password criteria
+const passwordCriteria = {
+    minLength: 6,
+    requireUppercase: true,
+    requireLowercase: true,
+    requireNumber: true,
+    requireSpecial: true,
+};
+
+const users = {};
+
+function signup(username, password){
+    if(users[username]){
+        // user exists
+        return { success: false, message: "User already exists" };
+    }
+    
+    if (!checkPasswordVal(password).valid) {
+        return { success: false, message: checkPasswordVal(password).message };
+    }
+
+    users[username] = { password };
+    return { success: true, message: "User registered successfully" };
+}
+
+function login(username, password){
+    if(!users[username]){
+        // user doesn't exist redirect to signup page if there
+
+        return { success: false, message: "User does not exist" };
+    }
+
+    if(users[username].password !== password){ // case sensitive
+        // incorrect password
+        return { success: false, message: "Incorrect password" };
+    }
+
+    return { success: true, message: "Login successful" };
+}
+
+function checkPasswordVal(password){
+
+    if(password.length < passwordCriteria.minLength || password.length > passwordCriteria.maxLength){
+        return { valid: false, message: "Password must be between 8 and 20 characters long." };
+    }
+
+    if(passwordCriteria.requireUppercase && !/[A-Z]/.test(password)){
+        return { valid: false, message: "Password must contain at least one uppercase letter." };
+    }
+
+    if(passwordCriteria.requireLowercase && !/[a-z]/.test(password)){
+        return { valid: false, message: "Password must contain at least one lowercase letter." };
+    }
+
+    if(passwordCriteria.requireNumber && !/[0-9]/.test(password)){
+        return { valid: false, message: "Password must contain at least one number." };
+    }
+
+    if(passwordCriteria.requireSpecial && !/[!@#$%^&*]/.test(password)){
+        return { valid: false, message: "Password must contain at least one special character." };
+    }
+
+    return { valid: true };
+}
---
+++ B01-RAP/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const fs = require("fs");
+
+animes = {
+    "Naruto": {
+        "title": "Naruto",
+        "id": 1,
+        "description": "A story about ninjas"
+    },
+    "Boku no Hero Academia": {
+        "title": "Boku no Hero Academia",
+        "id": 2,
+        "description": "A story about superheroes"
+    },
+    "One Piece": {
+        "title": "One Piece",
+        "id": 3,
+        "description": "A story about pirates"
+    }
+};
+
+console.log(animes);
+const app = express();
+
+app.get("/home", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/animes", (req, res) => {
+    
+    let html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Anime List</title>
+      </head>
+      <body>
+        <h1>Anime List</h1>
+        <ul>`;
+
+    Object.values(animes).forEach(anime => {
+        html += ` <li> ${anime.title} </li>`
+    });
+
+    html += `
+        </ul>
+      </body>
+    </html>`;
+    res.send(html);
+});
+
+app.get("/anime/:name", (req, res) => {
+    const anime = Object.values(animes).find(a => a.title.toLowerCase() === req.params.name.toLowerCase());
+
+    console.log(`${req.params.name}`);
+    if (anime) {
+        res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>${anime.title}</title>
+          </head>
+          <body>
+            <h1>${anime.title}</h1>
+            <p>${anime.description}</p>
+          </body>
+        </html>
+      `);
+    } else {
+        res.status(404).send("Anime not found");
+    }
+});
+
+
+app.post('/anime', (req, res) => {
+
+    if (!req.body.title || !req.body.description  || !req.body.id) {
+        return res.status(400).send("Missing id or title or description");
+    }
+    const { id, title, description } = req.body;
+
+  // save it in our array
+  animes.push({ id, title, description });
+    
+  console.log(animes);
+}) 
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
